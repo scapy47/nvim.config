24878378f5db4fc56524eecef22f086ebb6d5ce6
scapy_47 <134828740+Scapy47@users.noreply.github.com>
2025-03-27 13:05:29 +0530

    configured telescope
    ....
    
    ....
    
    ....
    
    ....

--- init.lua
+++ init.lua
@@ -67,6 +67,15 @@ end)
 -------------------------------
 -- Basic Keymaps
 -------------------------------
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+require("config.keymap").global()
 
 -------------------------------
 -- Install plugins & packages
